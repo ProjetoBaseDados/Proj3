28fbf13221043bfc4ccf526df492370108bfecac
JocostaNC <NC@github.com>
2017-11-24 18:37:53 +0000

    a

--- web/insert_super_Update.php
+++ web/insert_super_Update.php
@@ -26,7 +26,7 @@
                 $if = $db->query($sql1);
 
                 if ($if > 0) {
-
+                  
                 } else {
                   $sql = "UPDATE produto SET produto_categoria_name = 'Outros' WHERE categoria_name = '$categoria_name';";
                 }
