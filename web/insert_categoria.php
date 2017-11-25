3857c3ef062132ab0660eba3cc4aecf9b6db8141
JocostaNC <NC@github.com>
2017-11-25 23:27:59 +0000

    acho que ja esta as categorias!

---
+++ web/insert_categoria.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h3>Insert New Category</h3>
+        <form action="insert_categoria_Update.php" method="post">
+            <p>New Category: <input type="text" name="category_name"/></p>
+            <p><input type="submit" value="Submit"/></p>
+        </form>
+    </body>
+</html>
---
+++ web/insert_categoria_Update.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+<?php
+    $categoria_name = $_REQUEST['category_name'];
+    try
+    {
+        $host = "db.ist.utl.pt";
+        $user ="ist425904";
+        $password = "12345";
+        $dbname = $user;
+        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $db->query("start transaction;");
+
+        $sql = "INSERT INTO categoria values ('$categoria_name');";
+        $db->query($sql);
+        $sql = "INSERT INTO categoria_simples values ('$categoria_name');";
+        $db->query($sql);
+
+        echo("<p>$sql</p>");
+        
+        $db->query("commit;");
+
+        echo("<a href=\"mainMenu.php\">Go back to Main Menu</a>");
+
+        $db = null;
+    }
+    catch (PDOException $e)
+    {
+        $db->query("rollback;");
+        echo("<p>ERROR: {$e->getMessage()}</p>");
+    }
+?>
+    </body>
+</html>
