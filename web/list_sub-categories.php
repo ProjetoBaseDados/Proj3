0e007e5e942dea4c985d82095e5f013d182da864
JocostaNC <NC@github.com>
2017-11-25 22:21:33 +0000

    eheh

--- web/list_sub-categories.php
+++ web/list_sub-categories.php
@@ -1,7 +1,7 @@
 <html>
     <body>
         <h3>List sub-categories of super-category</h3>
-        <form action="list_sub_categories_Update.php" method="post">
+        <form action="list_sub-categories_Update.php" method="post">
             <p>Name of super-category: <input type="text" name="super-category"/></p>
             <p><input type="submit" value="Submit"/></p>
         </form>
