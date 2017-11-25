0f08dbae9c823786b69cb80684fdfb5b7b1bc9f7
vascomonizcunha <vascomonizcunha@gmail.com>
2017-11-25 20:53:28 +0000

    1a querie

--- queries.sql
+++ queries.sql
@@ -1,9 +1,6 @@
 select fornecedor_name from fornecedor natural join
 (select fornecedor_nif from
-(select (max(totCat)) from (fornecedor_nif count(fornecedor_name) as totCat from
-(select fornecedor_nif, produto_ean from fornecedor_sec,??? produto where produto_ean = produto_ean)
+(select max(totCat) from (fornecedor_nif count(fornecedor_name) as totalProduct from
+(select fornecedor_nif, produto_ean from fornecedor_sec Union
+select fornecedor_nif, produto_ean from produto)
 natural join produto) cross join (max(totCat)) where max(totCat) = totCat;
-
-
-ISTO TA UMA BELA MERDA VASCO <3
-VAI VER BEM OS NOMES DAS MERDAS AO SCHEMA :) HAVE FUN
