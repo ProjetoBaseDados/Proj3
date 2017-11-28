fde12757a871ac35f8b5af63c427b571afe956a8
JocostaNC <NC@github.com>
2017-11-28 18:41:24 +0000

    bota eheh

--- web/insert_product_with_Secondaries.php
+++ web/insert_product_with_Secondaries.php
@@ -2,12 +2,12 @@
     <body>
         <h2>Insert new product without secondary providers</h2>
         <form action="insert_product_without_Secondaries_Update.php" method="post">
-            <p>Product EAN: <input type="text" name="product_ean" pattern="(?=.13\d).{13}"/></p>
+            <p>Product EAN: <input type="text" name="product_ean"/></p>
             <p>Product Design: <input type="text" name="product_design"/></p>
             <p>Categoria name: <input type="text" name="category_name"/></p>
-            <p>Fornecedor Primario NIF: <input type="text" name="fornecedor_nif" pattern="(?=.9\d).{9}"/></p>
+            <p>Fornecedor Primario NIF: <input type="text" name="fornecedor_nif"/></p>
             <p>Fornecedor Primario name: <input type="text" name="fornecedor_name"/></p>
-            <p>Fornecedor Secundario NIF: <input type="text" name="fornecedor_sec_nif" pattern="(?=.9\d).{9}"/></p>
+            <p>Fornecedor Secundario NIF: <input type="text" name="fornecedor_sec_nif"/></p>
             <p>Fornecedor Secundario name: <input type="text" name="fornecedor_sec_name"/></p>
             <p><input type="submit" value="Submit"/></p>
         </form>
--- web/insert_product_without_Secondaries.php
+++ web/insert_product_without_Secondaries.php
@@ -2,10 +2,10 @@
     <body>
         <h3>Insert new product without secondary providers</h3>
         <form action="insert_product_without_Secondaries_Update.php" method="post">
-            <p>Product EAN: <input type="text" name="product_ean" pattern="(?=.13\d).{13}"/></p>
+            <p>Product EAN: <input type="text" name="product_ean"/></p>
             <p>Product Design: <input type="text" name="product_design"/></p>
             <p>Categoria name: <input type="text" name="category_name"/></p>
-            <p>Fornecedor Primario NIF: <input type="text" name="fornecedor_nif" pattern="(?=.9\d).{9}"/></p>
+            <p>Fornecedor Primario NIF: <input type="text" name="fornecedor_nif"/></p>
             <p>Fornecedor Primario name: <input type="text" name="fornecedor_name"/></p>
             <p><input type="submit" value="Submit"/></p>
         </form>
