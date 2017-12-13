ec896599baf9f70ea9c9f7bd2625a7346abcf6eb
Daniel Fortunato <dani.fortuna97@gmail.com>
2017-12-13 14:27:52 +0000

    coments trigger

--- triggers.sql
+++ triggers.sql
@@ -2,12 +2,14 @@
 -- Restrições de Integridade
 --------------------------------
 
+-- Trigger que verifica a insercao de dados na tabela dos fornecedores secundarios
+
 drop trigger if exists verifica_se_primario_trigger on fornecedor_sec;
 
 create or replace function verifica_se_primario() returns trigger as $$
 begin
 	if new.fornecedor_nif in (select fornecedor_nif from produto where produto_ean = new.produto_ean)
-		then raise exception 'O fornecedor % ja e primario deste produto', new.fornecedor_nif;
+		then raise exception 'O fornecedor % ja e primario do produto %', new.fornecedor_nif, new.produto_ean;
 	end if;
 	return new;
 End;
@@ -16,6 +18,7 @@ $$ Language plpgsql;
 create trigger verifica_se_primario_trigger after insert on fornecedor_sec
 for each row execute procedure verifica_se_primario();
 
+-- Trigger que verifica a insercao de dados na tabela dos fornecedores secundarios
 
 drop trigger if exists verifica_se_secundario_trigger on produto;
 
@@ -31,14 +34,14 @@ $$ Language plpgsql;
 create trigger verifica_se_secundario_trigger after update on produto
 for each row execute procedure verifica_se_secundario();
 
-
+-- Trigger que verifica se a data de reposicao é presente ou passada
 
 drop trigger if exists verifica_data_trigger on evento_reposicao;
 
 create or replace function verifica_data() returns trigger as $$
 begin
 	if DATEDIFF(timestamp, new.instante, GETDATE()) >= 0
-		then raise exception 'Impossivel repor no futuro, %', new.instante;
+		then raise exception 'A data actual é %, impossivel repor produtos em %.', now(), new.instante;
 	end if;
 	return new;
 End;
